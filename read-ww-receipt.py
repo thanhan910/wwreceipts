42c44b8155169f240bf185b263b11c2bc8a792c7
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-06-30 13:07:38 +1000

    Support reading online pdf files

--- read-ww-receipt.py
+++ read-ww-receipt.py
@@ -1,3 +1,5 @@
+import os
+import requests
 import sys
 import pdfplumber
 import polars as pl
@@ -324,12 +326,25 @@ def inspect_page_rectanges(pages):
 if __name__ == "__main__":
 
     pdf_path = sys.argv[1]
+    pdf_file_name = pdf_path.split('/')[-1].split('\\')[-1].split('.')[0]
     if len(sys.argv) > 2:
         output_path = sys.argv[2]
     else:
-        pdf_file_name = pdf_path.split('/')[-1].split('.')[0]
         output_path = f"{pdf_file_name}.csv"
     
+    using_temp_file = False
+    # Check if pdf_path is a web link or a local file
+    if pdf_path.startswith('http'):
+        print(f"Downloading {pdf_path}")
+        r = requests.get(pdf_path)
+        print(r.status_code, r.reason)
+        # Save the pdf file to a temporary file
+        using_temp_file = True
+        with open(f'{pdf_file_name}.pdf', 'wb') as f:
+            f.write(r.content)
+        pdf_path = f'{pdf_file_name}.pdf'
+
+
     with pdfplumber.open(pdf_path) as pdf:
         pages = {}
         page_count = len(pdf.pages)
@@ -348,7 +363,11 @@ if __name__ == "__main__":
             rects = page.rects
             words = page.extract_words()
 
+    if using_temp_file:        
+        os.remove(pdf_path)
+
     woolworths_summary, woolworths_tables = get_woolworths_data(pages)
     woolworths_tables_new = create_new_table(woolworths_tables)
     woolworths_tables_new_df = pl.DataFrame(woolworths_tables_new[1:], schema=woolworths_tables_new[0])
     woolworths_tables_new_df.write_csv(output_path)
+    print(woolworths_summary)
